26bb20c9975f3b129427691c875f1ec0acbe5486
Suman-Limbu <sumanscoopie123@gmail.com>
2026-01-16 22:35:39 +0545

    a

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,23 @@ import ComponentA from "./components/ComponentA";
 import ProductsTable from "./components/ProductsTable";
 
 export default function App() {
- return(<>
- </>)
-  
+  const [submit, setSubmit] = useState("");
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("submitting....");
+  };
+  return (
+    <>
+      {submit}
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={submit}
+          placeholder="submit your name"
+          onChange={(e) => setSubmit(e.target.value)}
+        />
+        <button type="submit">Login</button>
+      </form>
+    </>
+  );
 }
