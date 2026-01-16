61224abfac3326d464983d2eb954c728f8340ee1
Suman-Limbu <sumanscoopie123@gmail.com>
2026-01-16 22:09:44 +0545

    q

--- src/App.jsx
+++ src/App.jsx
@@ -5,32 +5,7 @@ import ComponentA from "./components/ComponentA";
 import ProductsTable from "./components/ProductsTable";
 
 export default function App() {
-  const [count, setCount] = useState(0);
-  useEffect(() => {
-    console.log(count);
-    console.log(titleRef);
-  }, [count]);
-  const inputRef = useRef(null);
-  const titleRef = useRef(null);
-  return (
-    <div>
-      <ProductsTable />
-      <span>count:{count}</span>
-      <ComponentA />
-
-      <button
-        onClick={() => {
-          inputRef.current.focus();
-          setCount(count + 1);
-        }}
-      >
-        click
-      </button>
-      <input type="text" id="one" ref={inputRef}></input>
-      <h1 ref={titleRef}>hello world</h1>
-      <Card title="Samsung Galaxy S24" brand="Samsung" price="1800" />
-      <Card title="onePlus Nord" brand="onePlus" price={1990} />
-      <Card title="Iphone 17" brand="Apple" price={1499} />
-    </div>
-  );
+ return(<>
+ </>)
+  
 }
