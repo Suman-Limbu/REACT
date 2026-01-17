c8dda991f1306f159eb9ea88570038644b5871c5
Suman-Limbu <sumanscoopie123@gmail.com>
2026-01-17 22:00:38 +0545

    a

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ const App = () => {
   return (
     <div>To-Do List
       <Input list={list} setList={setList}/>
+      {list}
     </div>
   )
 }
--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,12 +1,18 @@
 import React, { useState } from 'react'
 
-const Input = () => {
+const Input = ({list,setList}) => {
     const [todos,setTodos]=useState("");
+    
+    const handleChange=(e)=>{
+e.preventDefault();
+setList([...list,todos]);
+setTodos("");
+    }
   return (
     <>
     <form>
         <input type="text" placeholder="Add item"value={todos} onChange={(e)=>setTodos(e.target.value)}/>
-        <button >Add</button>
+        <button onClick={handleChange} >Add</button>
     </form>
     
     </>
