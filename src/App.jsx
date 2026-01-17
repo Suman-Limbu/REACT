a96d8f2e8e6d9b3c430f4882f84400ec12d7e3ec
Suman-Limbu <sumanscoopie123@gmail.com>
2026-01-17 22:23:07 +0545

    a

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React, { useState } from "react";
 import Input from "./components/input";
+import "./App.css"
 
 const App = () => {
   const [list, setList] = useState([]);
   return (
-    <div className="flex items-center justify-center">
+    <div className="flex flex-col items-center justify-center gap-5">
       <h1 className="">To-Do List</h1>
       <Input list={list} setList={setList} />
    <div>
