069d4c141266d2d65ca83dce5098669e7fb31ef7
Suman-Limbu <sumanscoopie123@gmail.com>
2026-06-22 22:51:43 +0545

    prac

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
-import counterReducer from "./counterSlice.js";
+import authReducer from "./auth/authSlice.js";
 
 export const store = configureStore({
-  reducer: { counter: counterReducer },
+  reducer: { auth: authReducer },
 });
