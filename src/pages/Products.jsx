647b03ea687d3be890a9299816f1d8daaa2f3dce
Suman-Limbu <sumanscoopie123@gmail.com>
2026-06-24 12:55:35 +0545

    prac

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,12 +1,9 @@
-import React from "react";
+import React from 'react'
 
 const Products = () => {
-const pro=fetch('https://dummyjson.com/products')
-.then(res => res.json())
-.then((data)=>data.products);
- console.log(pro)   
-
-  return <div>Products</div>;
-};
+  return (
+    <div>Products</div>
+  )
+}
 
 export default Products;
