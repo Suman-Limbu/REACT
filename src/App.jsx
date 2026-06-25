913cf8fd47c1ec705f017b4662f1357cb0c414c1
Suman-Limbu <sumanscoopie123@gmail.com>
2026-06-25 14:19:30 +0545

    prac

--- src/App.jsx
+++ src/App.jsx
@@ -7,22 +7,26 @@ import Home from "./pages/Home";
 import Course from "./pages/Course";
 import Memo from "./Memo";
 import Products from "./pages/Products";
-
-
+import ProtectedRoutes from "./Routes/ProtectedRoutes";
+import Login from "./pages/authPages/Login";
+import Unauthorized from "./pages/Unauthorized";
 
 const App = () => {
-const [count,setCount]=useState(0);
+  const [count, setCount] = useState(0);
   return (
     <div className="bg-red-400">
       <Navbar />
 
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/blogs" element={<Blogs />} />
+        <Route path="/login" element={<Login />} />
+         <Route path="/unauthorized" element={<Unauthorized />} />
         <Route path="/course" element={<Course />} />
-         <Route path="/products" element={<Products />} />
+        <Route element={<ProtectedRoutes allowedRoles={["Admi"]} />}>
+          <Route path="/products" element={<Products />} />
+           <Route path="/blogs" element={<Blogs />} />
+        </Route>
       </Routes>
-
     </div>
   );
 };
