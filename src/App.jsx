518df22ea7880d4fb3743ce9f58719d9e0d0bf15
Suman-Limbu <sumanscoopie123@gmail.com>
2026-07-17 11:25:10 +0545

    prac

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,16 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
-import React from 'react'
+import React, { useState } from "react";
 import Home from "./pages/Home";
-import Navbar from "./components/Navbar";
-import Products from "./pages/Products";
-import Blogs from "./pages/Blogs";
-import MainLayout from "./layout/MainLayout";
 
-const App = () => {
-  return (
+const App = () => {};
 
+return (
+  <div>
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
+  </div>
+);
 
-<div>
-   <Routes>
-
-<Route path="/" element={<Home/>}/>
-
-   </Routes>
-</div>
-  )
-}
-
-export default App
+export default App;
