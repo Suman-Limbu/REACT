3baa6546f6ed83a430a8229dd5ead2638acf4f92
Suman-Limbu <sumanscoopie123@gmail.com>
2026-07-30 14:02:35 +0545

    prac

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,11 @@
 import "./App.css";
 import React from "react";
 import MainLayout from "./layout/MainLayout";
-import ProductDetail from "./components/products/ProductDetail";
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Products from "./pages/Products";
+import ProductDetail from "./components/products/ProductDetail";
+import Cart from "./pages/Cart";
 
 
 const App = () => {
@@ -16,7 +17,7 @@ const App = () => {
           <Route path="/" element={<Home />} />
            <Route path="/products/" element={<Products />} />
              <Route path="/products/:id" element={<ProductDetail/>} />
-         
+           <Route path="/cart" element={<Cart/>} />
         </Route>
 
       </Routes>
--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,16 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 
 import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./redux/store.js";
 
 // import { ThemeProvider } from "./context/ThemeContext.jsx";
 // import  CartProvider  from "./context/CartContext.jsx";
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
-    <App />
+  <Provider store={store}>
+      <App />
+  </Provider>
   </BrowserRouter>,
 );
