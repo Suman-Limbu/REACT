e6fdc0941f458418a2dfa5fe59eff81a9761527c
Suman-Limbu <sumanscoopie123@gmail.com>
2026-08-02 11:06:11 +0545

    prac

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,44 +1,9 @@
-import Button from "@/components/ui/button";
-import Input from "@/components/ui/Input";
-import Select from "@/components/ui/Select";
-
-
-
-import React, { useState } from "react";
+import React from 'react'
 
 const Home = () => {
-  const countries = [
-    // {
-    //   label: "Nepal",
-    //   value: "np",
-    // },
-    // {
-    //   label: "India",
-    //   value: "in",
-    // },
-    // {
-    //   label: "USA",
-    //   value: "us",
-    // },
-
-    "apple",
-    "ball",
-    "cat",
-    "dog",
-    "cow",
-    "goat",
-  ];
-
-
-    const [country, setCountry] = useState("");
-
   return (
-    <>
-      <div className=" p-5 space-y-6">
-home
-      </div>
-    </>
-  );
-};
+    <div>Home</div>
+  )
+}
 
 export default Home;
